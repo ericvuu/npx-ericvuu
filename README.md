0d6875c60bcdfbf3ca8291dccd975904fad23182
Eric Vuu <ericlv91@gmail.com>
2023-04-27 10:36:11 -0700

    personal business card

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# npx-ericvuu
+Hi, I'm Eric!
+
+# Usage
+
+To see my business card execute the following:
+
+```bash
+npx ericvuu
+```
+
+![Eric Vuu's Business Card](business-card.PNG)
+
+# Credit
+
+Forked from [@bitandbang](https://twitter.com/bitandbang/status/1075473070368919552)
---
+++ bin/card.js
@@ -0,0 +1,56 @@
+#!/usr/bin/env node
+// 👆 Used to tell Node.js that this is a CLI tool
+
+// Pull in our modules
+const chalk = require('chalk')
+const boxen = require('boxen')
+
+// Define options for Boxen
+const options = {
+  padding: 1,
+  margin: 1,
+  borderStyle: 'round',
+}
+
+// Text + chalk definitions
+const data = {
+  name: chalk.white("Eric Vuu /"),
+  handle: chalk.cyan("ericvuu"),
+  work: chalk.white("Web Developer at Modelic"),
+  github: chalk.cyan("github.com/ericvuu"),
+  linkedin: chalk.cyan("linkedin.com/in/ericvuu/"),
+  web: chalk.cyan("https://ericvuu.dev/"),
+  npx: chalk.white("npx ericvuu"),
+  labelWork: chalk.white.bold("      Work:"),
+  labelGitHub: chalk.white.bold("    GitHub:"),
+  labelLinkedIn: chalk.white.bold("  LinkedIn:"),
+  labelWeb: chalk.white.bold("       Web:"),
+  labelCard: chalk.white.bold("      Card:"),
+};
+
+// Actual strings we're going to output
+const newline = '\n'
+const heading = `${data.name} ${data.handle}`
+const working = `${data.labelWork}  ${data.work}`
+const githubing = `${data.labelGitHub}  ${data.github}`
+const linkedining = `${data.labelLinkedIn}  ${data.linkedin}`
+const webing = `${data.labelWeb}  ${data.web}`
+const carding = `${data.labelCard}  ${data.npx}`
+
+// Put all our output together into a single variable so we can use boxen effectively
+const output =
+  heading +
+  newline +
+  newline +
+  working +
+  newline +
+  githubing +
+  newline +
+  linkedining +
+  newline +
+  webing +
+  newline +
+  newline +
+  carding
+
+console.log(chalk.green(boxen(output, options)))
